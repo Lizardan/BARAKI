6cee1b2372cee8f5ab9a13105d4cc1c8fcf06c2c
Lizardan <Lizard268@gmail.com>
2026-07-13 21:37:13 +0300

    Изменить пути к файлам Unity на сжатые версии

--- web/activity-shell/config.js
+++ web/activity-shell/config.js
@@ -8,7 +8,7 @@ window.BARAKI_CONFIG = {
   DEFAULT_PLAYER_COUNT: 2,
   UNITY_BUILD_URL: "./Build",
   UNITY_LOADER: "./Build/BARAKI.loader.js",
-  UNITY_DATA: "./Build/BARAKI.data",
-  UNITY_FRAMEWORK: "./Build/BARAKI.framework.js",
-  UNITY_CODE: "./Build/BARAKI.wasm",
+  UNITY_DATA: "./Build/BARAKI.data.gz",
+  UNITY_FRAMEWORK: "./Build/BARAKI.framework.js.gz",
+  UNITY_CODE: "./Build/BARAKI.wasm.gz",
 };
